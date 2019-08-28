f6ac249ec411a9173e2c196f2cb52e7fb122f8bc
markwhitfeld <markwhitfeld@users.noreply.github.com>
2019-08-28 21:43:45 +0200

    test: add advanced test scenarios for logger

--- packages/logger-plugin/tests/logger.plugin.spec.ts
+++ packages/logger-plugin/tests/logger.plugin.spec.ts
@@ -1,6 +1,6 @@
 import { ErrorHandler } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { throwError, of } from 'rxjs';
+import { throwError, of, merge } from 'rxjs';
 
 import { NgxsModule, Store, State, Action, StateContext } from '@ngxs/store';
 import { NoopErrorHandler } from '@ngxs/store/tests/helpers/utils';
@@ -8,7 +8,8 @@ import { StateClass } from '@ngxs/store/internals';
 
 import { NgxsLoggerPluginModule, NgxsLoggerPluginOptions } from '../';
 import { LoggerSpy } from './helpers';
-import { tap, delay } from 'rxjs/operators';
+import { tap, delay, flatMap } from 'rxjs/operators';
+import { patch } from '@ngxs/store/operators';
 
 describe('NgxsLoggerPlugin', () => {
   const GREY_STYLE = 'color: #9E9E9E; font-weight: bold';
@@ -282,5 +283,178 @@ describe('NgxsLoggerPlugin', () => {
 
     // Assert
     expect(logger.getCallStack()).toEqual([]);
+  });
+
+  it('should log success actions for nested actions (acceptance test)', async () => {
+    // Arrange
+
+    class Action1 {
+      static type = 'ACTION_1';
+    }
+    class Action2 {
+      static type = 'ACTION_2';
+    }
+    class Action3 {
+      static type = 'ACTION_3';
+    }
+    @State<StateModel>({ name: 'test', defaults: { bar: '' } })
+    class TestState {
+      @Action(Action1)
+      action1({ patchState, dispatch }: StateContext<StateModel>) {
+        patchState({ bar: 'A' });
+        dispatch(new Action2());
+      }
+
+      @Action(Action2)
+      action2({ setState }: StateContext<StateModel>) {
+        setState(patch({ bar: val => val + 'B' }));
+        // dispatch(new Action3());
+      }
+
+      @Action(Action3)
+      action3({ setState }: StateContext<StateModel>) {
+        setState(patch({ bar: val => val + 'C' }));
+      }
+    }
+
+    const { store, logger } = setup([TestState]);
+    logger.clear();
+
+    // Act
+    store.dispatch(new Action1());
+
+    // Assert
+    const expectedCallStack = [
+      ['group', 'action ACTION_1 (started @ )'],
+      [' log', '%c prev state', GREY_STYLE, { test: { bar: '' } }],
+      [' group', 'action ACTION_2 (started @ )'],
+      ['  log', '%c prev state', GREY_STYLE, { test: { bar: 'A' } }],
+      ['  log', '%c next state (synchronous)', GREEN_STYLE, { test: { bar: 'A' } }],
+      ['  log', '%c ( action doing async work... )', GREEN_STYLE, undefined],
+      [' groupEnd'],
+      [' group', '(async work completed) action ACTION_2 (started @ )'],
+      ['  log', '%c next state', GREEN_STYLE, { test: { bar: 'AB' } }],
+      [' groupEnd'],
+      [' log', '%c next state', GREEN_STYLE, { test: { bar: 'AB' } }],
+      ['groupEnd']
+    ];
+    expect(logger.getCallStack({ indent: true })).toEqual(expectedCallStack);
+  });
+
+  it('should log async success actions for nested actions (acceptance test)', async () => {
+    // Arrange
+
+    class AsyncAction1 {
+      static type = 'ASYNC_ACTION_1';
+    }
+    class AsyncAction1Inner {
+      static type = 'ASYNC_ACTION_1_INNER';
+    }
+    class AsyncAction2 {
+      static type = 'ASYNC_ACTION_2';
+    }
+    class AsyncAction2Inner {
+      static type = 'ASYNC_ACTION_2_INNER';
+    }
+    class AsyncAction3 {
+      static type = 'ASYNC_ACTION_3';
+    }
+    class AsyncAction3Inner {
+      static type = 'ASYNC_ACTION_3_INNER';
+    }
+    @State<StateModel>({ name: 'test', defaults: { bar: '' } })
+    class TestState {
+      @Action(AsyncAction1)
+      asyncAction1({ patchState, setState, dispatch }: StateContext<StateModel>) {
+        patchState({ bar: 'A' });
+        return merge(
+          dispatch(new AsyncAction2()),
+          of(null).pipe(
+            delay(1),
+            flatMap(() => {
+              setState(patch({ bar: val => val + '1' }));
+              return dispatch(new AsyncAction1Inner());
+            })
+          )
+        );
+      }
+
+      @Action(AsyncAction2)
+      asyncAction2({ setState, dispatch }: StateContext<StateModel>) {
+        setState(patch({ bar: val => val + 'B' }));
+        return merge(
+          dispatch(new AsyncAction3()),
+          of(null).pipe(
+            delay(1),
+            flatMap(() => {
+              setState(patch({ bar: val => val + '2' }));
+              return dispatch(new AsyncAction2Inner());
+            })
+          )
+        );
+      }
+
+      @Action(AsyncAction3)
+      asyncAction3({ setState, dispatch }: StateContext<StateModel>) {
+        setState(patch({ bar: val => val + 'C' }));
+        return of(null).pipe(
+          delay(1),
+          flatMap(() => {
+            setState(patch({ bar: val => val + '3' }));
+            return dispatch(new AsyncAction3Inner());
+          })
+        );
+      }
+    }
+
+    const { store, logger } = setup([TestState]);
+    logger.clear();
+
+    // Act
+    const promise = store.dispatch(new AsyncAction1()).toPromise();
+    logger.log('Some other work');
+    await promise;
+
+    // Assert
+    const expectedCallStack = [
+      ['group', 'action ASYNC_ACTION_1 (started @ )'],
+      [' log', '%c prev state', GREY_STYLE, { test: { bar: '' } }],
+      [' group', 'action ASYNC_ACTION_2 (started @ )'],
+      ['  log', '%c prev state', GREY_STYLE, { test: { bar: 'A' } }],
+      ['  log', '%c next state (synchronous)', GREEN_STYLE, { test: { bar: 'A' } }],
+      ['  log', '%c ( action doing async work... )', GREEN_STYLE, undefined],
+      [' groupEnd'],
+      [' group', 'action ASYNC_ACTION_3 (started @ )'],
+      ['  log', '%c prev state', GREY_STYLE, { test: { bar: 'AB' } }],
+      ['  log', '%c next state (synchronous)', GREEN_STYLE, { test: { bar: 'AB' } }],
+      ['  log', '%c ( action doing async work... )', GREEN_STYLE, undefined],
+      [' groupEnd'],
+      [' log', '%c next state (synchronous)', GREEN_STYLE, { test: { bar: 'ABC' } }],
+      [' log', '%c ( action doing async work... )', GREEN_STYLE, undefined],
+      ['groupEnd'],
+      ['log', 'Some other work'],
+      ['group', 'action ASYNC_ACTION_1_INNER (started @ )'],
+      [' log', '%c prev state', GREY_STYLE, { test: { bar: 'ABC1' } }],
+      [' log', '%c next state', GREEN_STYLE, { test: { bar: 'ABC1' } }],
+      ['groupEnd'],
+      ['group', 'action ASYNC_ACTION_2_INNER (started @ )'],
+      [' log', '%c prev state', GREY_STYLE, { test: { bar: 'ABC12' } }],
+      [' log', '%c next state', GREEN_STYLE, { test: { bar: 'ABC12' } }],
+      ['groupEnd'],
+      ['group', 'action ASYNC_ACTION_3_INNER (started @ )'],
+      [' log', '%c prev state', GREY_STYLE, { test: { bar: 'ABC123' } }],
+      [' log', '%c next state', GREEN_STYLE, { test: { bar: 'ABC123' } }],
+      ['groupEnd'],
+      ['group', '(async work completed) action ASYNC_ACTION_3 (started @ )'],
+      [' log', '%c next state', GREEN_STYLE, { test: { bar: 'ABC123' } }],
+      ['groupEnd'],
+      ['group', '(async work completed) action ASYNC_ACTION_2 (started @ )'],
+      [' log', '%c next state', GREEN_STYLE, { test: { bar: 'ABC123' } }],
+      ['groupEnd'],
+      ['group', '(async work completed) action ASYNC_ACTION_1 (started @ )'],
+      [' log', '%c next state', GREEN_STYLE, { test: { bar: 'ABC123' } }],
+      ['groupEnd']
+    ];
+    expect(logger.getCallStack({ indent: true })).toEqual(expectedCallStack);
   });
 });
